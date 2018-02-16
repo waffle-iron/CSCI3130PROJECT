49e4293527122fdd113294a71b2ce7dd38fe1ed8
Guo Yunfei <philipguo1992@gmail.com>
2018-02-16 12:59:34 -0400

    Regular code<login in with firebase><yiwei zhang, yunfei guo>

--- app/src/main/java/com/example/yiweizhang/csci3130project/LoginActivity.java
+++ app/src/main/java/com/example/yiweizhang/csci3130project/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.example.yiweizhang.csci3130project;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    
+    private FirebaseAuth mAuth;
     private EditText userEnterEmail, userEnterPassword;
     private Button userClickSignIn;
 
@@ -42,22 +50,15 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            //Log.d(TAG, "signInWithEmail:success");
+
                             FirebaseUser user = mAuth.getCurrentUser();
 
-                            Toast.makeText(MainActivity.this, "sign in success.",
+                            Toast.makeText(LoginActivity.this, "sign in success.",
                                     Toast.LENGTH_SHORT).show();
 
-                            // start a new activity when login successful
-                            // could be the profile edit activity
-
-
-
                         } else {
-                            // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
+
+                            Toast.makeText(LoginActivity.this, "sign in failed.",
                                     Toast.LENGTH_SHORT).show();
 
                         }
