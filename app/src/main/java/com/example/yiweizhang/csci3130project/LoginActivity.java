45fa8b84db1c428fd45e0f812ac1a66a813afe88
yiwei <yw349762@dal.ca>
2018-02-24 15:22:40 -0400

    Regular Code<Login Page><yiwei zhang  yunfei guo>

--- app/src/main/java/com/example/yiweizhang/csci3130project/LoginActivity.java
+++ app/src/main/java/com/example/yiweizhang/csci3130project/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.yiweizhang.csci3130project;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
 
         userEnterEmail = findViewById(R.id.userEnteredEmail);
         userEnterPassword = findViewById(R.id.userEnteredPassword);
@@ -42,16 +44,20 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void userWantSignIn(){
-        String userEmail = userEnterEmail.getText().toString();
+        final String userEmail = userEnterEmail.getText().toString();
         String userPassword = userEnterPassword.getText().toString();
+
+
+
 
         mAuth.signInWithEmailAndPassword(userEmail, userPassword)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
+
                         if (task.isSuccessful()) {
 
-                            FirebaseUser user = mAuth.getCurrentUser();
 
                             Toast.makeText(LoginActivity.this, "sign in success.",
                                     Toast.LENGTH_SHORT).show();
@@ -63,7 +69,6 @@ public class LoginActivity extends AppCompatActivity {
 
                         }
 
-                        // ...
                     }
                 });
     }
