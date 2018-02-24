4974beb709983a86e8b56b970fa084b28a0ef0ab
alan chan <en542509@dal.ca>
2018-02-24 17:34:13 -0400

    original craft code(not done)

---
+++ app/src/main/java/com/example/yiweizhang/csci3130project/Course.java
@@ -0,0 +1,93 @@
+package com.example.yiweizhang.csci3130project;
+
+/**
+ * Created by chenenlin on 18/2/24.
+ */
+
+public class Course {
+        String name;
+        String location;
+        int section;
+        String day;
+        int time;
+        int courseHour;
+        int courseId;
+        String instructor;
+
+        public Course(String name, String location,int section,String day,int time,int courseHour,int courseId,String instructor){
+            this.name = name;
+            this.location = location;
+            this.section = section;
+            this.day = day;
+            this.time = time;
+            this.courseHour = courseHour;
+            this.courseId = courseId;
+            this.instructor = instructor;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public int getSection() {
+            return section;
+        }
+
+        public void setSection(int section) {
+            this.section = section;
+        }
+
+        public String getDay() {
+            return day;
+        }
+
+        public void setDay(String day) {
+            this.day = day;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public void setTime(int time) {
+            this.time = time;
+        }
+
+        public int getCourseHour() {
+            return courseHour;
+        }
+
+        public void setCourseHour(int courseHour) {
+            this.courseHour = courseHour;
+        }
+
+        public int getCourseId() {
+            return courseId;
+        }
+
+        public void setCourseId(int courseId) {
+            this.courseId = courseId;
+        }
+
+        public String getInstructor() {
+            return instructor;
+        }
+
+        public void setInstructor(String instructor) {
+            this.instructor = instructor;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
