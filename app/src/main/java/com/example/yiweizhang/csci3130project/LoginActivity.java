6756d23c32479c7a6b2149db055b058c9a173f07
Guo Yunfei <philipguo1992@gmail.com>
2018-02-24 14:50:20 -0400

    regular code <login > <yiwei zhang, yunfei guo>

--- app/src/main/java/com/example/yiweizhang/csci3130project/LoginActivity.java
+++ app/src/main/java/com/example/yiweizhang/csci3130project/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.yiweizhang.csci3130project;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -55,6 +56,10 @@ public class LoginActivity extends AppCompatActivity {
 
                             Toast.makeText(LoginActivity.this, "sign in success.",
                                     Toast.LENGTH_SHORT).show();
+
+                            // start the MainActivity
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
 
                         } else {
 
